d4ad53c8cefa92aae2dba28f175448ee5e65f137
ethan-ssd <ethanmrich@gmail.com>
2022-11-21 14:05:54 +0100

    Add message builder and test

--- loopit/wrapper.py
+++ loopit/wrapper.py
@@ -1,26 +1,15 @@
-# how to import Client from the loop it codebase?
-
-
 '''
-python ipc/tests/rcc.py --msg
-                          
-python __init__.py --msg '{"tft":{"0":{"ip_address": null}}}'                                  
-python __init__.py --msg '{"fes": ("O": {"new parameters": "available"}}}'    
-python __init__.py --msg '{"fes" : {"O" {"current mode": {"biphasic": {"amplitude A" :null}}}}}'
-python __init__.py --msg '["fes" : ("O": ("state mosi": "stimulate once"]]]'
-python __init__.py --msg '("fes": {"O": ("state mosi": "stop"})]'
-
 order of the JSON message nesting is:
-module name: "fes"
-module identifier: "0"
-mode name: "current_mode"
-parameter name: "inter_pulse_interval"
-parameter value: 4000000000
+{"module_name": {"idx": {"mode_name": {"parameter_name": "parameter_value:<string, int, boolean>"}}}}
 
-e.g. set the inter-pulse interval to 4 seconds
+-> set the inter-pulse interval to 4 seconds
 '{"fes": {"0": {"current_mode": {"inter_pulse_interval": "4000000000"}}}}'
 
+-> trigger one stimulation
+'{"fes": {"0": {"current_mode": {"state mosi": "stimulate_once"}}}}'
 
+-> trigger one stimulation
+'{"fes": {"0": {"current_mode": {"state mosi": "stop"}}}}'
 
 '''
 
@@ -28,15 +17,18 @@ from loopit.rcc import Client
 
 class LoopIT(Client):
 ### Further simplified methods for use in research setting
-
-    def format_message_json(msg=r'{"ping":"pong"}'):
-        # take a message from the user and format to JSON structure expected by the Loop IT
-        pass
+    
+    def set_mode(self, module_name, module_index, mode_name):
+        self.module_name, self.module_index, self.mode_name = module_name, module_index, mode_name
 
     def query(self):
         self.request()
 
-    def set_parameters(self, msg=r'{"fes": {"0": {"current_mode": {"inter_pulse_interval": "4000000000"}}}}'):
+    def set_parameters(self, parameter, value):
+        
+        msg = '{' + self.module_name + ': {' + self.module_index + ': {' + self.mode_name +  \
+        ': {' + parameter + ': '+ value +'}}}}'
+        
         self.request(msg=msg)
 
     def start_stimulation():
@@ -44,3 +36,5 @@ class LoopIT(Client):
 
     def stop_stimulation():
         pass
+    
+    
--- test.py
+++ test.py
@@ -4,11 +4,15 @@ if __name__ == "__main__":
 
     # initialize the client
     loopit = LoopIT(host='127.0.0.1', port=1219)
+    loopit.set_mode(module_name = "fes",
+                    module_index= "0",
+                    mode_name = "current_mode")
     
     # query the Loop IT
     loopit.query()
     # set some parameters
-    loopit.set_parameters()
+    loopit.set_parameters(parameter = "inter_pulse_interval", value = "4000000000")
     # start stimulation
-    
+    loopit.set_parameters(parameter = "state_mosi", value = "stimulate_once")
     # stop stimulation
+    loopit.set_parameters(parameter = "state_mosi", value = "stop")
