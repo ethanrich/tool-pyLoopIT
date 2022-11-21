796673b70d3b470cc8a68ac80714019aba5d16dd
ethan-ssd <ethanmrich@gmail.com>
2022-11-21 11:20:22 +0100

    Add test file

--- loopit/__init__.py
+++ loopit/__init__.py
@@ -1,15 +1,1 @@
 from loopit.wrapper import LoopIT
-
-# if __name__ == "__main__":
-
-#     # initialize the client
-#     client = LoopIT(host='127.0.0.1', port=1219)
-    
-#     # query the Loop IT
-    
-#     # set some parameters
-    
-#     # start stimulation
-    
-#     # stop stimulation
-#     client.request()
