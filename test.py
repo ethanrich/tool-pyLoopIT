be6e97f88c198e4210b92fda9b2a86654030c1f3
ethan-ssd <ethanmrich@gmail.com>
2022-11-21 14:19:07 +0100

    Update test example

--- test.py
+++ test.py
@@ -5,14 +5,18 @@ if __name__ == "__main__":
     # initialize the client
     loopit = LoopIT(host='127.0.0.1', port=1219)
     loopit.set_mode(module_name = "fes",
-                    module_index= "0",
+                    module_index = "0",
                     mode_name = "current_mode")
     
     # query the Loop IT
     loopit.query()
     # set some parameters
-    loopit.send_message(parameter = "inter_pulse_interval", value = "4000000000")
+    loopit.send_message(parameter = "inter_pulse_interval", value = "20000000") # 50 hz == 20000000 nanosecond period
+    loopit.send_message(parameter = "amplitude_A", value = "1000000") # one milliampere of current
+    loopit.send_message(parameter = "amplitude_A", value = "-1000000")
+    loopit.send_message(parameter = "pulsewidth_A", value = "200000") # 200 microsecond pulse width
+    loopit.send_message(parameter = "pulsewidth_B", value = "200000")
     # start stimulation
-    loopit.send_message(parameter = "state_mosi", value = "stimulate_once")
+    loopit.send_message(parameter = "state_mosi", value = "stimulate_continuous")
     # stop stimulation
     loopit.send_message(parameter = "state_mosi", value = "stop")
