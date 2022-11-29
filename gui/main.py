156ad63fad1098a49e03d8eeb5d4e80ec5c4665e
ethan-ssd <ethanmrich@gmail.com>
2022-11-29 15:18:10 +0100

    Fix start and stop callbacks

--- gui/main.py
+++ gui/main.py
@@ -83,10 +83,25 @@ def send_to_loopit_callback():
         send.configure(fg="red", activeforeground="red", text="Please set parameters")
         
 def start_stimulation_callback():
+    status_stimulation("on")
     loopit.start_stimulation()
 
 def stop_stimulation_callback():
+    status_stimulation("off")
     loopit.stop_stimulation()
+    
+def status_stimulation(msg):
+    global status
+    status = msg
+    if status == "off":
+        start["state"] = "normal"
+        stop["state"] = "disabled"
+        canvas.itemconfig(info, text="STIM OFF", fill="white")
+        
+    elif status == "on":
+        stop["state"] = "normal"
+        start["state"] = "disabled"
+        canvas.itemconfig(info, text="STIM ON", fill="red", font=('Impact', -50))
 
 send = tk.Button(text="Send to LoopIT", font=("Roboto-Bold", 16), borderwidth=3, highlightthickness=0, relief="raised", height=5, width=20, command=send_to_loopit_callback)
 
@@ -98,32 +113,7 @@ stop["state"] = "disabled"
 info = canvas.create_text(400.0, 650, text="Start Stimulation", fill="black", font=("Roboto-Medium", 20))
 
 
-######## Set Stim status
-def start_stimulation():
-    status_playing("on")
-
-def stop_stimulation():
-    status_playing("off")
-
-def status_playing(yeter):
-    global status
-    status = yeter
-    if status == "off":
-        start["state"] = "normal"
-        stop["state"] = "disabled"
-        canvas.itemconfig(info, text="STIM OFF", fill="white")
-        
-    elif status == "on":
-        stop["state"] = "normal"
-        start["state"] = "disabled"
-        canvas.itemconfig(info, text="STIM ON", fill="red", font=('Impact', -50))
-
-
-
 ######## Event loop
-start.config(command=lambda: start_stimulation())
-stop.config(command=lambda: stop_stimulation())
-
 running = True
 while running:
     root.update()
@@ -135,7 +125,3 @@ while running:
     start.place(x=218, y=675, width=172, height=58)
     stop.place(x=418, y=675, width=172, height=58)
     root.config(menu=menubar)
-    if status == "on":
-        start_stimulation()
-    elif status == "off":
-        pass
